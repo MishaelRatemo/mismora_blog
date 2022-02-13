09bbbc278c4c6c4f5a12d026d5f77deff633a122
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-13 19:04:00 +0300

    db initializing

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from config import config_options
 
+from flask_sqlalchemy import SQLAlchemy
+
+# database initialization
+db = SQLAlchemy()
+
+
 
 def create_app(config_name):
     app = Flask(__name__)
     
     app.config.from_object(config_options[config_name])
+    
+    #flask extensions initialization
+    db.init_app(app)
     
     #  registration  of Blueprint
     from .main import  root as app_blueprint
