6462423c2849b40df34a39758f635ead45ed2f01
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-13 18:43:33 +0300

    App initialization

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from config import config_options
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    
+    app.config.from_object(config_options[config_name])
+    
+    #  registration  of Blueprint
+    from .main import  root as app_blueprint
+    app.register_blueprint(app_blueprint)
+    
+    return app 
+     
