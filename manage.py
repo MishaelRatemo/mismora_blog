1fbf24d9f1ac0f36d821b1f2a42a2e85b3040637
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-14 08:26:00 +0300

    Added shell for migrations

--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
 from app import  create_app
 from flask_script import Manager, Server
+from app import db
+from app.models import Users,Posts,Comments, Likes,Unlikes
+from flask_migrate import Migrate,MigrateCommand
 # app instances
 app = create_app('development_mode')
 # app = create_app('development_mode')
@@ -7,6 +10,17 @@ app = create_app('development_mode')
 manager = Manager()
 manager.add_command('server', Server)
 
+@manager.shell
+def make_shell_context():
+    return dict(
+                    app =app,
+                    db=db,
+                    Users=Users,
+                    Posts=Posts,
+                    Comments =Comments,
+                    Likes=Likes,
+                    Unlikes = Unlikes        
+                )
 
 if __name__ == '__main__':
     manager.run()
