1dee50960a52c5157917a1ce6cb7eec4e07a093d
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-17 13:35:37 +0300

    App configurations

--- config.py
+++ config.py
@@ -1,21 +1,30 @@
+from distutils.debug import DEBUG
 import os
 
 from click import confirmation_option
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'secretkey'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/mismorablog'
     
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
 class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
+    DEBUG=True
     pass
 class TestConfig(Config):
     pass
 config_options = {
-    'development_mode' : DevConfig,
+    'development' : DevConfig,
     'production_mode' :ProdConfig,
     'testing' : TestConfig
 }
